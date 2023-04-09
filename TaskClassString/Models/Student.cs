9583b557f66138c1d604da27af8eb0282be32f6d
Ali Taghizada <alitaqizade2@gmail.com>
2023-04-09 17:50:38 +0400

    added task

---
+++ TaskClassString/Models/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskClassString.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Group { get; set; }
+        public double Point { get; set; }
+        public bool IsGraduated { get; set; }
+
+
+        public Student()
+        {
+            
+        }
+
+        public Student(string name, string surname, string group,
+            double point, bool isGraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group; 
+            Point = point;
+            IsGraduated = isGraduated;
+        }
+
+        
+        public string GetFullName()
+        {
+            return $"{Name} {Surname}";
+        }
+
+        public string AdmissionToTheExam()
+        {
+            if(Point > 80)
+            {
+                return "Can take the exam";
+            }
+            else
+            {
+                return "Cannot take the exam";
+            }
+        }
+
+        public string GetInfo()
+        {
+            Console.WriteLine("{");
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Surname: " + Surname);
+            Console.WriteLine("Group: " + Group);
+            Console.WriteLine("Point: " + Point);
+            Console.WriteLine("IsGraduated: " + IsGraduated);
+            Console.WriteLine("}");
+
+            if (IsGraduated == true)
+            {
+                return "Graduated";
+            }
+            else
+            {
+                return "Not graduated";
+            }
+        }
+    }
+}
---
+++ TaskClassString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using TaskClassString.Models;
+
+Student[] students = new Student[3];
+students[0] = new Student("Ali", "Taghizada", "AB201", 89.4, true);
+students[1] = new Student("Ali", "Safarli", "AB201", 78.4, false);
+students[2] = new Student("Hikmat", "Habibli", "AP208", 84.3, false);
+
+
+StringBuilder result = new StringBuilder();
+Console.WriteLine(result.Append(students[0].GetInfo()));
+
+
+StringBuilder result2 = new StringBuilder();
+Console.WriteLine(result2.Append(students[1].GetInfo()));
+
+
+StringBuilder result3 = new StringBuilder();
+Console.WriteLine(result3.Append(students[2].GetInfo()));
