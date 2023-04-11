92fd4426ee5cdab4d82a67b3fdd51a42249710c4
Ali Taghizada <alitaqizade2@gmail.com>
2023-04-11 08:05:24 +0400

    added task

---
+++ StringMethods/CustomString.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethods
+{
+    public class CustomString
+    {
+        public string Empty = "";
+
+
+        public bool StartWith(string str, char checkValue)
+        {
+            if (str[0] == checkValue)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool EndWith(string str, char checkValue)
+        {
+            if (str[str.Length - 1] == checkValue)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int Length(string str)
+        {
+            int count = 0;
+            foreach (var item in str)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public int IndexOf(string str, char checkValue)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == checkValue)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int LastIndexOf(string str, char checkValue)
+        {
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                if (str[i] == checkValue)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public string Replace(string str, char replacement1, char replacement2)
+        {
+            StringBuilder sb = new StringBuilder(str);
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == replacement1)
+                {
+                    sb[i] = replacement2;
+                }
+            }
+            return sb.ToString();
+        }
+
+        public string Remove(string str, int beginIndex)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < beginIndex; i++)
+            {
+                result += str[i];
+            }
+            return result;
+        }
+
+        public string SubString(string str, int startIndex, int length)
+        {
+            string result = string.Empty;
+            for (int i = startIndex; i < length; i++)
+            {
+                result += str[i];
+            }
+            return result;
+        }
+
+        public string Trim(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ StringMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using StringMethods;
+
+CustomString customString = new CustomString();
+
+//string.Empty
+string text = customString.Empty;
+
+//StartWith
+string word = "Hello World";
+Console.Write("StartWith: ");
+Console.WriteLine(customString.StartWith(word, 'H'));
+
+//EndWith
+Console.Write("EndWith: ");
+Console.WriteLine(customString.EndWith(word, 'e'));
+
+//Length
+Console.Write("Length: ");
+Console.WriteLine(customString.Length(word));
+
+//IndexOf
+Console.Write("IndexOf: ");
+Console.WriteLine(customString.IndexOf(word, 'W'));
+
+//LastIndexOf
+Console.Write("LastIndexOf: ");
+Console.WriteLine(customString.LastIndexOf(word, 'o'));
+
+//Replace
+Console.Write("Replace: ");
+Console.WriteLine(customString.Replace(word, 'o', 'Z'));
+
+//Remove
+Console.Write("Remove: ");
+Console.WriteLine(customString.Remove(word, 3));
+
+//SubString
+Console.Write("SubString: ");
+Console.WriteLine(customString.SubString(word, 1, 8));
+
+//Trim
+string word2 = "   Ali    ";
+Console.WriteLine("Trim: ");
+Console.Write(customString.Trim(word2));
