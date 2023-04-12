5bb0dc25588947389429b77689f16d44b796ef9c
Ali Taghizada <alitaqizade2@gmail.com>
2023-04-12 05:31:28 +0400

    added task

---
+++ TaskAccessModifiersEncaptulation/Person.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAccessModifiersEncaptulation
+{
+    internal class Person
+    {
+
+        #region Encapsulation
+        //private string Name { get; set; }
+        //private string Surname { get; set; }
+        //private int Age { get; set; }
+        //private int BirthDay { get; set; }
+
+
+        //public void SetName(string name)
+        //{
+        //    if (name.Length < 2)
+        //    {
+        //        Console.WriteLine("Wrong value for name");
+        //        return;
+        //    }
+        //    Name = name;
+        //}
+
+        //public string GetName() { return Name; }
+
+        //public void SetAge(int age)
+        //{
+        //    if (age < 16)
+        //    {
+        //        Console.WriteLine("You are not old enough");
+        //        return;
+        //    }
+        //    Age = age;
+        //}
+
+        //public int GetAge() { return Age; }
+
+        #endregion
+
+        #region OvverideGetSet
+        private string _name;
+        private string _surname;
+        private int _age;
+        private int _birthDay;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (value.Length < 2)
+                {
+                    Console.WriteLine("Wrong value for name");
+                    return;
+                }
+                _name = value;
+            }
+        }
+
+        public string Surname { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 16)
+                {
+                    Console.WriteLine("You are not old enough");
+                    return;
+                }
+                _age = value;
+            }
+        }
+
+        public int BirthDay { get; set; }
+        #endregion
+    }
+}
---
+++ TaskAccessModifiersEncaptulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using TaskAccessModifiersEncaptulation;
+
+Person person1 = new Person();
+
+//Default get, set
+//person1.SetName("Ali");
+//Console.WriteLine(person1.GetName());
+
+//person1.SetAge(15);
+//Console.WriteLine(person1.GetAge());
+
+
+//Ovveride get, set
+//person1.Name = "Test";
+//person1.Age = 17;
+
+//Console.WriteLine(person1.Name);
+//Console.WriteLine(person1.Age);
