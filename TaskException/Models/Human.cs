403b3cfcc768afb7c811578257ae91ba4276e0c0
Ali Taghizada <alitaqizade2@gmail.com>
2023-04-20 05:12:27 +0400

    first commit

---
+++ TaskException/Models/Human.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskException.Utilities.Exceptions;
+
+namespace TaskException.Models
+{
+    internal class Human
+    {
+        public Human(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        private int _age;
+        public int Age 
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new AgeException("Incorrect value for age");
+                }
+                _age = value;
+            }
+        }
+    }
+}
---
+++ TaskException/Program.cs
@@ -0,0 +1,9 @@
+﻿using TaskException.Models;
+
+Human ali =  new Human("Ali", "Taghizada", 21);
+Console.WriteLine($"Name: {ali.Name} \nSurname: {ali.Surname} " +
+    $"\nAge: {ali.Age}");
+
+
+//Exception
+Human ali2 = new Human("Ali", "Taghizada", 0);
