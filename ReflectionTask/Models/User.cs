1bf1e8d642c3fb90e8e9dace29da147302bd791f
AliTH1 <116202591+AliTH1@users.noreply.github.com>
2023-04-22 14:53:59 +0400

    added task

---
+++ ReflectionTask/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionTask.Models
+{
+    internal class User
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private static int Age { get; set; }
+
+
+        public void GetFullName()
+        {
+            Console.WriteLine("{0} {1}", Name, Surname);
+        }
+
+        public static void ChangeAge(int age)
+        {
+            Age = age;
+        }
+    }
+}
---
+++ ReflectionTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using ReflectionTask.Models;
+using System.Reflection;
+
+//Assembly assembly = Assembly.GetExecutingAssembly();
+//Type type = assembly.GetType(typeof(User).ToString());
+Type type = typeof(User);
+
+User user1 = Activator.CreateInstance(typeof(User)) as User;
+
+PropertyInfo propId = type.GetProperty("Id", BindingFlags.Instance|BindingFlags.NonPublic);
+PropertyInfo propName = type.GetProperty("Name", BindingFlags.Instance | BindingFlags.NonPublic);
+PropertyInfo propSurname = type.GetProperty("Surname", BindingFlags.Instance | BindingFlags.NonPublic);
+PropertyInfo propAge = type.GetProperty("Age", BindingFlags.Static | BindingFlags.NonPublic);
+
+propId.SetValue(user1, 5);
+propName.SetValue(user1, "Ali");
+propSurname.SetValue(user1, "Taghizada");
+propAge.SetValue(null, 21);
+
+Console.WriteLine("Id: " + propId.GetValue(user1));
+Console.WriteLine("Name: " + propName.GetValue(user1));
+Console.WriteLine("Surname: " + propSurname.GetValue(user1));
+Console.WriteLine("Age: " + propAge.GetValue(user1));
+
+
+MethodInfo methodGetFullName = type.GetMethod("GetFullName");
+Console.Write("\nFull name: ");
+methodGetFullName.Invoke(user1, null);
+
+MethodInfo methodChangeAge = type.GetMethod("ChangeAge");
+methodChangeAge.Invoke(user1, new object[] { 25 });
+Console.WriteLine("Age changed");
+Console.WriteLine("Age: " + propAge.GetValue(user1));
